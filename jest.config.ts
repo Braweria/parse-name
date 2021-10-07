11eab223a139f7ca8497da80b70420e5d1a89da9
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-10-07 11:58:53 +0200

    🤡 configure jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "@jest/types";
+
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: false,
+};
+
+export default config;
