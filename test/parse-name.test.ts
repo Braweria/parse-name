fe3b60a25da41da0636a5d62e3daa1477dfc2dc2
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-10-08 13:36:07 +0200

    ✅ add passing tests

--- test/parse-name.test.ts
+++ test/parse-name.test.ts
@@ -1,15 +1,68 @@
 import { parseName } from "../lib/parse-name";
 
-test("parse prefix only", () => {
+test("parse full name", () => {
   expect(parseName("Frau Maria Höfer")).toEqual({
     prefix: "Frau",
-    first: null,
+    first: "Maria",
+    middle: null,
     initials: null,
     infix: null,
-    last: null,
+    last: "Höfer",
     suffix: null,
     full: "Frau Maria Höfer",
   });
 });
 
-// Frau Maria Höfer
+test("parse full name", () => {
+  expect(parseName("Wiktoria Mielcarek")).toEqual({
+    prefix: null,
+    first: "Wiktoria",
+    middle: null,
+    initials: null,
+    infix: null,
+    last: "Mielcarek",
+    suffix: null,
+    full: "Wiktoria Mielcarek",
+  });
+});
+
+test("parse full name", () => {
+  expect(parseName("Dr. Raphael M. van der Börk II")).toEqual({
+    prefix: "Dr.",
+    first: "Raphael",
+    middle: null,
+    initials: "M.",
+    infix: "van der",
+    last: "Börk",
+    suffix: "II",
+    full: "Dr. Raphael M. van der Börk II",
+  });
+});
+
+test("parse full name", () => {
+  expect(parseName("Prof. Steph Alice von Wolfenstein")).toEqual({
+    prefix: "Prof.",
+    first: "Steph",
+    middle: "Alice",
+    initials: null,
+    infix: "von",
+    last: "Wolfenstein",
+    suffix: null,
+    full: "Prof. Steph Alice von Wolfenstein",
+  });
+});
+
+test("parse full name", () => {
+  expect(
+    parseName("Königin Elizabeth Alexandra Mary Mountbatten-Windsor")
+  ).toEqual({
+    prefix: "Königin",
+    first: "Elizabeth",
+    middle: "Alexandra",
+    initials: null,
+    infix: null,
+    last: "Mountbatten-Windsor",
+    suffix: null,
+    full: "Königin Elizabeth Alexandra Mary Mountbatten-Windsor",
+  });
+});
